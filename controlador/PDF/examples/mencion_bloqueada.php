12d5e859fcefe7369cee6da76cf02e1991e5b37b
Danny De La Rosa <dannydelarosa18@hotmail.com>
2017-03-01 14:12:36 -0400

    Add files via upload

---
+++ controlador/PDF/examples/mencion_bloqueada.php
@@ -0,0 +1,20 @@
+<?php
+    ob_start();
+    include(dirname(__FILE__).'/res/mencion_bloqueada.php');
+    $content = ob_get_clean();
+
+    // convert in PDF
+    require_once(dirname(__FILE__).'/../html2pdf.class.php');
+    try
+    {
+        $html2pdf = new HTML2PDF('P', 'letter', 'ES');
+//      $html2pdf->setModeDebug();
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+        $html2pdf->Output('Reporte de mencion_bloqueada.pdf');
+    }
+    catch(HTML2PDF_exception $e) {
+        echo $e;
+        exit;
+    }
+?>
