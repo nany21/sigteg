6e6fb950f2734b8f9e1e3eaf031c884074657571
Danny De La Rosa <dannydelarosa18@hotmail.com>
2017-03-01 14:07:47 -0400

    Add files via upload

---
+++ controlador/PDF/controlador/PDF/examples/usuario.php
@@ -0,0 +1,20 @@
+<?php
+    ob_start();
+    include(dirname(__FILE__).'/res/usuario.php');
+    $content = ob_get_clean();
+
+    // convert in PDF
+    require_once(dirname(__FILE__).'/../html2pdf.class.php');
+    try
+    {
+        $html2pdf = new HTML2PDF('P', 'letter', 'ES');
+//      $html2pdf->setModeDebug();
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+        $html2pdf->Output('Reporte de usuario.pdf');
+    }
+    catch(HTML2PDF_exception $e) {
+        echo $e;
+        exit;
+    }
+?>
